17cc7a2e471b94bfe33fe6c7628fff6005e6d5c9
Dorian Thompson <thompsondorian2@gmail.com>
2022-05-18 22:20:43 -0400

    refactor passport js code to use async await instead of fetch

--- services/passport.js
+++ services/passport.js
@@ -24,19 +24,15 @@ passport.use(
             callbackURL: '/auth/google/callback',
             proxy: true
         }, 
-        (accessToken, refreshToken, profile, done) => {
-            //initial check to see they is an existing query with this google id
-            User.findOne({ googleId: profile.id})
-                .then((existingUser) => {
-                    if(existingUser){
-                        done(null, existingUser);
-                    }else{
-                        //new model instance and save it to DB
-                        new User({ googleId: profile.id }).save()
-                            .then((user) =>  done(null, user));
-                    }
-                }
-            );
-        }
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleId: profile.id})
+                
+            if(existingUser){
+                return done(null, existingUser);
+            }
+
+            const user = await new User({ googleId: profile.id }).save();
+            done(null, user);   
+        }   
     )
 );
